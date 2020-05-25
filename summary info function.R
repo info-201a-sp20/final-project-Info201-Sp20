fb6c84526fa96b72e8e21d59b87922faf2a1dc44
John <jhenzler@uw.edu>
2020-05-24 17:33:11 -0700

    index commit 2

--- summary info function.R
+++ summary info function.R
@@ -1,4 +1,42 @@
-## Summary Information 
+# Takes in the data and the column of interest. Returns info about the
+# highest total revenue made by an artist.
+get_artist_revenue <- function(df, col) {
+  result <- df %>% 
+    group_by(artist) %>%
+    summarise(revenue = sum(indicativerevenue)) %>%
+    filter(revenue == max(revenue, na.rm = TRUE)) %>%
+    pull(as.name(col))
+  if (typeof(result) == "double") {
+    result <- round(result)
+  }
+  return(result)
+}
+
+
+
+# Takes in the data, the column of interest, and a min/max function. Returns
+# info about the song earning the most/least revenue in one year.
+get_song_revenue <- function(df, col, fun) {
+  result <- df %>% 
+    filter(indicativerevenue == match.fun(fun)(indicativerevenue)) %>%
+    pull(as.name(col))
+  if (typeof(result) == "double") {
+    result <- round(result)
+  }
+  return(result)
+}
+
+# Takes in the data and the column of interest. Returns info about the
+# artist who appeared the most number of times.
+get_artist_most_appeared <- function(df, col) {
+  result <- df %>% 
+    group_by(artist) %>%
+    count() %>% 
+    arrange(-n) %>% 
+    head(1) %>% 
+    pull(as.name(col))
+
+}
 
 chart_2000 <- read.csv("data/chart2000-songyear-0-3-0058.csv", stringsAsFactors = FALSE)
 summary_function <- function(df){
@@ -16,32 +54,51 @@ summary_function <- function(df){
   
   artist_most_appeared <- chart_2000 %>% 
     group_by(artist) %>% 
+    workspacejh
     count() %>%
     arrange(-n) %>%
-    head(1) %>% 
-    pull(artist)
+    head(1) %>%
+    pull(as.name(col))
+  return(result)
+}
+
+# Takes in the data. Returns a list where the keys describe the values, and
+# the values are summary pieces about the dataset.
+get_summary_info <- function(df){
+
+  artist_most_revenue <- get_artist_revenue(df, "artist")
+  artist_revenue_total <- get_artist_revenue(df, "revenue")
   
-  song_least_revenue_in_one_year <- chart_2000 %>% 
-    filter(indicativerevenue == min(indicativerevenue)) %>% 
-    pull(song)
+  song_most_revenue <- get_song_revenue(df, "song", "max")
+  song_most_revenue_year <- get_song_revenue(df, "year", "max")
   
-  song_most_appeared <- chart_2000 %>% 
+  artist_most_appeared <- get_artist_most_appeared(df, "artist")
+  times_artist_appeared <- get_artist_most_appeared(df, "n")
+  
+  song_least_revenue <- get_song_revenue(df, "song", "min")
+  
+  song_most_appeared <- df %>% 
     group_by(song) %>% 
     count() %>% 
     arrange(-n) %>% 
     head(2) %>% 
     pull(song)
   
-  
   my_list <- list("Artist with the most total revenue" = artist_most_revenue,
-                  "Song wth the most revenue in one year" = song_most_revenue_in_one_year,
+                  "Artist's total revenue" = artist_revenue_total,
+                  "Song with the most revenue in one year" = song_most_revenue,
+                  "Year that occured" = song_most_revenue_year,
                   "Artist who appeared the most times" = artist_most_appeared,
-                  "Song with the least revenue in one year" = song_least_revenue_in_one_year,
+                  "Times they appeared" = times_artist_appeared,
+                  "Song with the least revenue in one year" = 
+                   song_least_revenue,
                   "Song that appeared the most times" = song_most_appeared)
   
   return(my_list)
 }
 
 df <- chart_2000
-summary_list <- summary_function(df)
 
+j <- get_summary_info
+
+summary_list <- summary_function(df)
--- summary table function.R
+++ summary table function.R
@@ -1,4 +1,12 @@
+
+# Takes in the data. Returns a table with the top artists of each year
+# between 2000-2020, sorted in decending order.
+
 chart_2000 <- read.csv("data/chart2000-songyear-0-3-0058.csv", stringsAsFactors = FALSE)
+
+
+chart_2000 <- read.csv("data/chart2000-songyear-0-3-0058.csv", stringsAsFactors = FALSE)
+
 summary_table_function <- function(df){
   
   top_artist_yearly <- df %>% 
@@ -8,7 +16,14 @@ summary_table_function <- function(df){
     arrange(-year)
     
   return(top_artist_yearly)
-  
 }
+
+
 df <- chart_2000
+
 summary_table <- summary_table_function(df)
+ 
+summary_table
+
+summary_table <- summary_table_function(df)
+
