280994a27be6d3c1ef4469acc5b4993aae4ad117
Abigail Mendoza <mendoabi@uw.edu>
2020-06-02 12:34:35 -0700

    remove setwd line and lint

--- Shiny_app/genre_density.R
+++ Shiny_app/genre_density.R
@@ -1,51 +1,46 @@
+# TOP ARTIST GENRE DENSITY
+# Takes in genre(s) and generates a pie chart showing how many artists belong
+# in each genre and how they compare to other genres.
 
 # Load relevant libraries
 library(shiny)
 library(dplyr)
-library(plotly)
 library(ggplot2)
-
-setwd("C:/Users/Abi/Desktop/final project/final-project-Info201-Sp20")
-
+library(plotly)
 
 # Read in genre data
-genredf <- read.csv('Data/charts_w_genres.csv', stringsAsFactors = FALSE)
+genredf <- read.csv("Data/charts_w_genres.csv", stringsAsFactors = FALSE)
 
-#Subset data frame in order to show only genre and number of artists in
+# Subset data frame in order to show only genre and number of artists in
 # defined genre
 genredf_stripped <- subset(genredf, select = -c(year, position, song,
-                                                indicativerevenue, X)) %>% 
+                                                indicativerevenue, X)) %>%
     group_by(genre) %>%
     summarize(num_artist = n())
 
-
-# Define UI 
+# Define UI
 ui <- fluidPage(
     titlePanel("Genre Density: Which Genres Do Top Artists Belong To?"),
     sidebarLayout(
         sidebarPanel(
-            selectInput("genre",label = "Choose genres to display:",
+            selectInput("genre", label = "Choose genres to display:",
                                choices = genredf_stripped$genre, multiple = T),
             h4("Analysis"),
             p("Insert why this is significant data here")
         ),
         mainPanel(
-            mainPanel(plotlyOutput("PiePlot"))
+            mainPanel(plotlyOutput("pieplot"))
         )
     )
 )
 
-
 #Define Server
-
-
 server <- function(input, output, session) {
-    output$PiePlot <- renderPlotly({
+    output$pieplot <- renderPlotly({
         # Retrieve user input from shiny widget
-        user_selections <- genredf_stripped %>% 
+        user_selections <- genredf_stripped %>%
             subset(genre %in% input$genre)
-        
-        # Pie Chart visual formatting 
+        # Pie Chart visual formatting
         font <- list(
             size = 15,
             color = "white"
@@ -55,33 +50,24 @@ server <- function(input, output, session) {
             bordercolor = "transparent",
             font = font
         )
-        # Example artists - not implemented yet! Can't get it to show up
-        # in hoverbox correctly.
-        
-        #example_artists <- genredf %>% 
-            #subset(genre %in% input$genre) %>% 
-            #pull(artist) %>%
-            #sample(3) %>%
-            #paste()
 
         # Pie Chart
-        PiePlot <- plot_ly(user_selections,
+        pieplot <- plot_ly(user_selections,
                            labels = ~genre,
                            values = ~num_artist,
-                           type = 'pie',
+                           type = "pie",
                            hoverlabel = label,
                            hovertemplate = paste(
                                "<b>Percent: </b>%{percent}",
                                "<br><b># of Artists:</b>",
                                "%{value}<br>",
                                "<extra></extra>"))  %>%
-            layout(title = 'Top Artist Density in Genre Categories',
+            layout(title = "Top Artist Density in Genre Categories",
                    xaxis = list(showgrid = FALSE, zeroline = FALSE,
                                 showticklabels = FALSE),
                    yaxis = list(showgrid = FALSE, zeroline = FALSE,
                                 showticklabels = FALSE))
-        
     })
 }
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
