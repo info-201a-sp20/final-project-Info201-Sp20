b0cf6bfe9168e8ff1adc9c00759a8099f90544ee
Kayla Perez <perezkay@uw.edu>
2020-06-07 00:49:42 -0700

    forgot to add lines of code that work with input

--- Shiny_app/build_chart1.R
+++ Shiny_app/build_chart1.R
@@ -13,8 +13,12 @@ plot_genre_trends <- function(df) {
   with_zeros <- non0_genres %>%
     spread(genre, times, fill = 0) %>%
     gather(genre, times, -year)
+ 
+  filtered <- with_zeros %>%
+    filter(year >= input$year[1], year <= input$year[2])
+  filtered$times <- filtered$times * 10
 
-  fig <- plot_ly(with_zeros, x = ~year, y = ~times, type = "scatter",
+  fig <- plot_ly(filtered, x = ~year, y = ~times, type = "scatter",
                   mode = "line", color = ~genre, colors = colorRampPalette(
                     brewer.pal(5, "Dark2"))(20)) %>%
     layout(title = "Frequency of Genres Appearing Each Year - Top 10 Artists",
