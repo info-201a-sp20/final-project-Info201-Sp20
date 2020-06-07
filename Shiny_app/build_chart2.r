0ff909a55989d99c3a47d92fce4b597d677cf1e1
Kayla Perez <perezkay@uw.edu>
2020-06-06 23:58:59 -0700

    linted

--- Shiny_app/build_chart2.r
+++ Shiny_app/build_chart2.r
@@ -16,33 +16,33 @@ add_solo_artists <- function(main_df) {
 
 # Takes in the charts dataframe WITH solo artist column. Returns a new dataframe
 #that contains artist and number of hits they had.
-count_num_hits <- function(df){
+count_num_hits <- function(df) {
   df_new <- add_solo_artists(df)
-  times_appeared <- df_new %>% 
-    group_by(solo_artist) %>% 
+  times_appeared <- df_new %>%
+    group_by(solo_artist) %>%
     summarize(num_hits = n())
   return(times_appeared)
 }
 
 
 # Takes a number of hits, a filtered (artist + number of hits) dataframe, and
-# the main charts dataframe. Returns a dataframe with the artist column, 
+# the main charts dataframe. Returns a dataframe with the artist column,
 # number of hits column matching given param, and their respective genre.
 make_hits_table <- function(num, filtered, data) {
   # this is times
-  top_num_hits <- filtered %>% 
+  top_num_hits <- filtered %>%
     filter(num_hits == num)
-  
+
   if (as.integer(num) <= 5) {
     top_num_hits <- sample_n(top_num_hits, 15)
   }
-  
+
   artists <- top_num_hits$solo_artist
   artist_genres <- list()
   for (i in seq(length(artists))) {
-    genre <- data %>% # this is from read.csv
-      filter(solo_artist == as.name(artists[[i]])) %>% 
-      head(1) %>% 
+    genre <- data %>%
+      filter(solo_artist == as.name(artists[[i]])) %>%
+      head(1) %>%
       pull(genre)
     artist_genres[[i]] <- genre
   }
@@ -56,11 +56,11 @@ make_hits_table <- function(num, filtered, data) {
 # Takes in a number of hits table, and the number of hits. Returns a bar chart
 # showing the frequency of each genre in the given table.
 plot_genre_frequency <- function(table, num_hits) {
-  plot_table <- table %>% 
-    group_by(Genre) %>% 
+  plot_table <- table %>%
+    group_by(Genre) %>%
     summarize(frequency = n())
-  
-  fig <- plot_ly(data=plot_table, x=~Genre, y=~frequency, type="bar")
+
+  fig <- plot_ly(data = plot_table, x = ~Genre, y = ~frequency, type = "bar")
   fig <- fig %>%
     layout(title = paste0("Frequency of Genres for Artists with ",
                            num_hits, " Hits"),
@@ -68,6 +68,3 @@ plot_genre_frequency <- function(table, num_hits) {
            xaxis = list(tickangle = 45))
   return(fig)
 }
-
-
-
